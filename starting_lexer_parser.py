82f1aedcb5cd869a5f07f2c088be00597153cf85
asamufaja <54369675+asamufaja@users.noreply.github.com>
2022-09-29 14:22:03 -0600

    Add files via upload

--- starting_lexer_parser.py
+++ starting_lexer_parser.py
@@ -6,14 +6,43 @@ from sly import Lexer, Parser
 import math
 
 class CalcLexer(Lexer):
-    tokens = { NAME, NUMBER, PLUS, TIMES, MINUS, DIVIDE, ASSIGN, LPAREN, RPAREN, FACTORIAL }
+    tokens = { IDENT, NUMBER, PLUS, TIMES, MINUS, DIVIDE, ASSIGN, LPAREN, RPAREN, NOT, 
+            BOOL, BREAK, CASE, CLASS, CHAR, CIN, COUT, DEFAULT, ELSE, FALSE, IF, INT, KXI2022, NEW, 
+            NULL, PUBLIC, PRIVATE, RETURN, STRING, SWITCH, TRUE, VOID, WHILE
+            }
     ignore = ' \t'
 
     # Tokens
-    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    IDENT = r'[a-zA-Z_][a-zA-Z0-9_]*'
     NUMBER = r'\d+'
 
-    # Special symbols
+    # keywords
+    BOOL = r'bool'
+    BREAK = r'break'
+    CASE = r'case'
+    CLASS = r'class'
+    CHAR = r'char'
+    CIN = r'cin'
+    COUT = r'cout'
+    DEFAULT = r'default'
+    ELSE = r'else'
+    FALSE = r'false'
+    IF = r'if'
+    INT = r'int'
+    KXI2022 = r'kxi2022'
+    NEW = r'new'
+    NULL = r'null'
+    PUBLIC = r'public'
+    PRIVATE = r'private'
+    RETURN = r'return'
+    STRING = r'string'
+    SWITCH = r'switch'
+    TRUE = r'true'
+    VOID = r'void'
+    WHILE = r'while'
+
+    # symbols  : ; { } [ ] == != >= <= > < && || += -= *= /= << >> . ,
+    # COLON, SEMICOLON, LBRACE, RBRACE, LBRACKET, RBRACKET, EQUALS, NOTEQUALS, GREATEROREQUAL, LESSOREQUAL, GREATERTHAN, LESSTHAN, AND, OR, PLUSEQUALS, MINUSEQUALS, TIMESEQUALS, DIVIDEEQUALS, LEFTSHIFT, RIGHTSHIFT, PERIOD, COMMA
     PLUS = r'\+'
     MINUS = r'-'
     TIMES = r'\*'
@@ -21,7 +50,7 @@ class CalcLexer(Lexer):
     ASSIGN = r'='
     LPAREN = r'\('
     RPAREN = r'\)'
-    FACTORIAL = r'!'
+    NOT = r'!'
 
     # Ignored pattern
     ignore_newline = r'\n+'
@@ -39,16 +68,16 @@ class CalcParser(Parser):
 
     precedence = (
         ('left', PLUS, MINUS),
-        ('left', TIMES, DIVIDE, FACTORIAL),
-        ('right', UMINUS),
+        ('left', TIMES, DIVIDE),
+        ('right', UMINUS, NOT),
         )
 
     def __init__(self):
-        self.names = { }
+        self.idents = { }
 
-    @_('NAME ASSIGN expr')
+    @_('IDENT ASSIGN expr')
     def statement(self, p):
-        self.names[p.NAME] = p.expr
+        self.idents[p.NAME] = p.expr
 
     @_('expr')
     def statement(self, p):
@@ -70,7 +99,7 @@ class CalcParser(Parser):
     def expr(self, p):
         return p.expr0 / p.expr1
 
-    @_('expr FACTORIAL')
+    @_('expr NOT')
     def expr(self, p):
         return math.factorial(p.expr)
 
@@ -86,12 +115,12 @@ class CalcParser(Parser):
     def expr(self, p):
         return int(p.NUMBER)
 
-    @_('NAME')
+    @_('IDENT')
     def expr(self, p):
         try:
-            return self.names[p.NAME]
+            return self.idents[p.IDENT]
         except LookupError:
-            print(f'Undefined name {p.NAME!r}')
+            print(f'Undefined name {p.IDENT!r}')
             return 0
 
 if __name__ == '__main__':
